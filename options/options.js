e2cadfd2a261ea8d8314ed802c820621a57e6929
Crowdbotics <team@crowdbotics.com>
2024-11-15 18:31:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-dust-50058",
+  url: "https://delicate-dust-50058.botics.co",
+  api: "https://delicate-dust-50058.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
